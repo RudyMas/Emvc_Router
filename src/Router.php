eff550165badf73142fac93bf86317b630be9ba4
Rudy Mas <rudy.mas@rudymas.be>
2020-05-09 14:27:25 +0200

    Bugfix: Fixed a PSR-4 mistake

--- src/Router.php
+++ src/Router.php
@@ -12,7 +12,7 @@ use Mobile_Detect;
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2018-2020, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.1.1.0
+ * @version     1.1.1.1
  * @package     EasyMVC
  */
 class Router
